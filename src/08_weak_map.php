46c89c2bce50015d841390856c927be56a94d236
Dominik <dominik.stypula1@gmail.com>
2022-12-17 10:45:39 +0100

    Update 08_weak_map.php

--- src/08_weak_map.php
+++ src/08_weak_map.php
@@ -1,4 +1,6 @@
 <?php
+///////////////// FIRST EXAMPLE /////////////////////////////
+
 class User { private $name = 'john'; }
 
 $wm = new WeakMap();
@@ -15,3 +17,31 @@ print_r($wm);
 unset($obj);
 
 print_r($wm);
+
+///////////////// SECOND EXAMPLE /////////////////////////////
+
+class User { public $name = 'john'; }
+
+class Test { 
+	public function get(object $o): object { 
+		return $o; 
+	}
+}
+
+$wm1 = new WeakMap();
+$wm2 = new WeakMap();
+
+$u1 = new User;
+$u2 = (new Test())->get($u1);
+
+var_dump(spl_object_hash($u1) === spl_object_hash($u2));
+
+$wm1[$u1] = [1,2,3];
+
+$wm2[$u2] = [4,5,6];
+
+unset($u1);
+
+// despite that $u1 is the $u2 object (returned from Test::get()) and has the spl_object_hash()
+// $wm1 and $wm2 is NOT cleared
+var_dump($wm1, $wm2);
